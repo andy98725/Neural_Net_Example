ab24e7aece70e6a7c4eceb84481bae96478f96dd
Andy Hudson <andyhudson725@gmail.com>
2018-03-05 14:11:54 -0600

    Added missing files

---
+++ src/math/sigFunctions.cpp
@@ -0,0 +1,19 @@
+/*
+ * sigFunctions.cpp
+ *
+ *  Created on: Mar 2, 2018
+ *      Author: Andy
+ */
+
+#include "sigFunctions.h";
+
+float sigmoid(float in){
+  return 1 / (1 + exp (in));
+}
+
+float sigmoidPrime(float in){
+  return sigmoid(in) * (1-sigmoid(in));
+}
+
+
+
---
+++ src/math/sigFunctions.h
@@ -0,0 +1,18 @@
+/*
+ * sigFunctions.h
+ *
+ *  Created on: Mar 2, 2018
+ *      Author: Andy
+ */
+
+#ifndef SIGFUNCTIONS_H_
+#define SIGFUNCTIONS_H_
+
+#include<math.h>
+
+float sigmoid(float);
+float sigmoidPrime(float);
+
+
+
+#endif /* SIGFUNCTIONS_H_ */
